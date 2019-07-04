e464f121381257cb78e7d38b6e9d949a31cd4d9b
Miguel Maia <migueljunior09@hotmail.com>
2019-07-04 09:58:04 -0300

    Updates

---
+++ precache-manifest.711c538b30c99252aa7038640ec5d675.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fb4c75eab0c2e7e47c0fd31aee42bb6",
+    "url": "/git-compare/index.html"
+  },
+  {
+    "revision": "8ec2e4be0a6d8b2af488",
+    "url": "/git-compare/static/css/2.a05569e8.chunk.css"
+  },
+  {
+    "revision": "8ec2e4be0a6d8b2af488",
+    "url": "/git-compare/static/js/2.813eedcc.chunk.js"
+  },
+  {
+    "revision": "a5a8fd66da2c098359d7",
+    "url": "/git-compare/static/js/main.4ead7402.chunk.js"
+  },
+  {
+    "revision": "e9b91a8e98e8751bd3fc",
+    "url": "/git-compare/static/js/runtime~main.84d43ad0.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/git-compare/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/git-compare/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/git-compare/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/git-compare/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/git-compare/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
